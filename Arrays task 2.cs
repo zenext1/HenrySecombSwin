fa57049f062cd881fd4f628c017cae7afa91e972
zenext1 <63435730+zenext1@users.noreply.github.com>
2020-04-11 13:24:08 +1000

    Add files via upload

---
+++ Arrays task 2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0};
+
+            int sum = 0;
+            foreach (int item in array)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(sum);
+            
+            
+                
+                
+            
+            
+
+            
+
+           
+
+                
+
+                
+            
+            
+                
+            
+
+           
+
+             
+
+            
+        }
+    }
+} 
