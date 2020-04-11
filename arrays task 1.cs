eaa0e86b9cc863092b4e223eebf25bc3ea11ee54
zenext1 <63435730+zenext1@users.noreply.github.com>
2020-04-11 15:00:41 +1000

    Add files via upload

---
+++ arrays task 1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace new_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        
+        {
+            
+            
+        
+        
+            string [] names = new string[5];
+            names[0] = "Daniel";
+            names[1] = "Will";
+            names[2] = "Henry";
+            names[3] = "David";
+            names[4] = "Nik";
+
+
+            Console.WriteLine("hello" + names[0]);
+            Console.WriteLine("hello" + names[1]);
+            Console.WriteLine("hello" + names[2]);
+            Console.WriteLine("hello" + names[3]);
+            Console.WriteLine("hello" + names[4]);
+            
+
+
+
+            
+            
+            
+        
+
+            
+            
+        
+        }
+    }
+}
